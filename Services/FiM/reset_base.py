001f8941839c9d929796c5a00e96d18292614988
Juan Camilo <112782217+Donkami04@users.noreply.github.com>
2025-01-08 16:50:46 -0500

    Fix bug fim: No actualizaba status prtg, agregado time_read_out (#66)

--- Services/FiM/reset_base.py
+++ Services/FiM/reset_base.py
@@ -1,6 +1,7 @@
 import logging
 import traceback
 from netmiko import ConnectHandler
+import os
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -8,13 +9,16 @@ logging.basicConfig(
 
 file_handler = logging.FileHandler("issues.log")
 file_handler.setLevel(logging.WARNING)
-file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+file_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+)
 logging.getLogger().addHandler(file_handler)
 paramiko_logger = logging.getLogger("paramiko")
 paramiko_logger.setLevel(logging.WARNING)
 
 
 def reset_base(ip_base):
+    session_log = f"session_{ip_base.replace('.', '_')}.log"
 
     try:
         logging.info(f"Iniciando Reinicio {ip_base}")
@@ -24,21 +28,39 @@ def reset_base(ip_base):
             "username": "admin",
             "password": "netman",
             "port": 23,
+            "session_log": session_log,  # Guarda el log de la sesión
+            "fast_cli": False,  # Evita configuraciones rápidas
+            "timeout": 60,  # Aumentar el tiempo de espera general
+            "blocking_timeout": 60,  # Tiempo de espera para operaciones bloqueantes
+            "read_timeout_override": 60,  # Sobrescribir el timeout de lectura
         }
-        
+
         # Iniciar la conexión TELNET
         with ConnectHandler(**device) as telnet:
-            output = telnet.send_command("reboot")
-            telnet.disconnect()
-            logging.info(f"Output {output}")
+            # Desactiva los ajustes automáticos de Netmiko
+            telnet.disable_paging(
+                command="set cli pager off"
+            )  # Evita comandos como "terminal length 0"
+
+            # Envía el comando de reinicio
+            output = telnet.send_command("reboot", delay_factor=5)
+            logging.info(f"Output: {output}")
             logging.info(f"Base reiniciada en {ip_base}")
-        
+
         return "OK", "OK"
-            
+
     except Exception as e:
-        logging.error("Error en funcion Reset_Base")
+        logging.error("Error en función Reset_Base")
         logging.error(e)
         logging.error(traceback.format_exc())
         return "ERROR", e
-        
-# reset_base("10.224.89.10")
+
+    finally:
+        error_log = f"error_{ip_base.replace('.', '_')}.log"
+        if os.path.exists(session_log):
+            os.rename(session_log, error_log)
+            logging.error(f"Log detallado guardado en: {error_log}")
+
+
+# Ejemplo de uso
+# reset_base("10.224.89.13")
